2b0eede918573c75e8d07139d566ab6e1362d364
Mattias <dalebjer88@gmail.com>
2025-11-01 15:14:27 +0100

    feat(AboutSection) Added AboutSection with phonelink and hovereffects

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import AboutSection from '../components/AboutSection';
 
 export default function HomePage() {
   return (
     <>
       {/* WCAG: SR-only */}
       <h2 className="sr-only">Startsida - StorAid</h2> 
-
+      <AboutSection/>
      
     </>
   );
